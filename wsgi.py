1319b0db3de1d5ec19db6c7f38a6e75d6e2c3a79
adamkong1995 <adamkong256@gmail.com>
2019-06-22 19:09:06 +0800

    replace key.py with heroku env variable

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,13 @@
 from flaskr.app import app
 import sys
-from flaskr import key
 
-if app.config['ENV'] == 'development':
-    app.run(debug=True)
-elif app.config['ENV'] == 'production':
-    sys.path.append(key.PROJECT_PATH)
-    from flaskr.app import app as application
+app.run()
+
+# Use it if you have key.py file
+#from flaskr import key
+
+# if app.config['ENV'] == 'development':
+#     app.run(debug=True)
+# elif app.config['ENV'] == 'production':
+#     sys.path.append(key.PROJECT_PATH)
+#     from flaskr.app import app as application
